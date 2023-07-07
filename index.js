697869b0465b5c2e4688dbcfab28bb55186891b5
Kosovircek <mitja.gracar94@gmail.com>
2023-07-07 02:12:19 +0200

    Gif in README

--- index.js
+++ index.js
@@ -1,39 +1,104 @@
 import {ControlEdge, CopyEdge} from "./src/edge.js";
+import {Tile} from "./src/tile.js";
 
-let edges = new Array();
 let svg = document.getElementById("plane");
 
-let edge1 = new ControlEdge(svg);
-edge1.addBezier([[-50,0], [-50,-10], [50,-10], [50,0]]);
-edge1.translate(300, 300);
-edge1.translate(0, -50);
-edge1.redraw();
-let copyEdge1 = new CopyEdge(svg);
-copyEdge1.translate(300, 300);
-copyEdge1.rotate(1.5708*2);
-copyEdge1.translate(0, -50);
-copyEdge1.mirror();
-
-let edge2 = new ControlEdge(svg);
-edge2.addBezier([[-50,0], [-50,-10], [50,-10], [50,0]]);
-edge2.translate(300, 300);
-edge2.rotate(1.5708);
-edge2.translate(0, -50);
-edge2.redraw();
-let copyEdge2 = new CopyEdge(svg);
-copyEdge2.translate(300, 300);
-copyEdge2.rotate(1.5708*3);
-copyEdge2.translate(0, -50);
-copyEdge2.mirror();
-
-edges.push(edge1);
-edges.push(edge2);
+let tile1 = new Tile(svg, [
+  {
+    curve: [[-50,0], [-50,-10], [50,-10], [50,0]],
+    x: 0,
+    y: 0,
+    rotation: 0
+  },
+  {
+    mirror: true,
+    x: 50,
+    y: -50,
+    rotation: toRad(-90)
+  },
+  {
+    mirror: false,
+    x: 95,
+    y: -125,
+    rotation: toRad(-30)
+  },
+  {
+    mirror: true,
+    x: 163,
+    y: -107,
+    rotation: toRad(60)
+  },
+  {
+    mirror: false,
+    x: 162,
+    y: -20,
+    rotation: toRad(120)
+  },
+  {
+    mirror: true,
+    x: 180,
+    y: 48,
+    rotation: toRad(30)
+  },
+  {
+    mirror: false,
+    x: 224,
+    y: 125,
+    rotation: toRad(90)
+  },
+  {
+    mirror: false,
+    x: 174,
+    y: 176,
+    rotation: toRad(0)
+  },
+  {  
+    mirror: true,
+    x: 99,
+    y: 219,
+    rotation: toRad(-60)
+  },
+  {  
+    mirror: false,
+    x: 31,
+    y: 237,
+    rotation: toRad(30)
+  },
+  {  
+    mirror: true,
+    x: -56,
+    y: 237,
+    rotation: toRad(-30)
+  },
+  {  
+    mirror: false,
+    x: -124,
+    y: 219,
+    rotation: toRad(60)
+  },
+  {  
+    mirror: false,
+    x: -124,
+    y: 133,
+    rotation: toRad(-60)
+  },
+  {  
+    mirror: true,
+    x: -75,
+    y: 46,
+    rotation: toRad(-60)
+  },
+]);
+function toRad(degrees) {
+  var pi = Math.PI;
+  return degrees * (pi/180);
+}
 
 globalRedraw();
 
 export function globalRedraw() {
   console.log("global redraw called");
-  edges.forEach(e => e.redraw());
-  copyEdge1.redraw(edge1.segments);
-  copyEdge2.redraw(edge2.segments);
+  tile1.redraw();
 }
+
+
